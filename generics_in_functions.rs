64b9b659fc707d32aee9fcc5b5e958da33adae94
Julio <julio.cabrapan@hotmail.com>
2019-08-31 13:39:19 +0200

    Update generics_in_functions.rs

--- generics_in_functions.rs
+++ generics_in_functions.rs
@@ -1,14 +1,14 @@
-/* Generic types to remove duplication of code -
-by extracting a function    */
+/* GENERIC TYPES: 
+- to remove duplication of code by extracting a function.   */
 
-// 1) Short program that finds the largest number in a list:    ----------------------------------------------------------------------------
+// 1) Short program that finds the largest number in a list:
 
 fn main() {
     let number_list= vec! [34, 50, 25, 100, 65]; 
 
     let mut largest_number= number_list [0]; 
     
-    for number in number_list {         // number_list values are moved here, std::vec::Vec<i32> doesn't implement the `Copy` trait
+    for number in number_list {             //number_list values are moved here, std::vec::Vec<i32> doesn't implement the `Copy` trait -
         if number > largest_number {
             largest_number= number; 
         }
@@ -22,9 +22,10 @@ To eliminate this duplication, create an abstraction by defining a function that
 2. Extract the duplicate code into the body of the function and specify the inputs and return values of that code in the function signature.
 3. Update the two instances of duplicated code to call the function instead.    */ 
 
-// 2) Finding the largest number in a list abstractly       ----------------------------------------------------------------------------- 
-    fn largest (list: &[i32]) -> i32 {           // [i32] represents any concrete slice of i32 values 
-        let mut largest= list [0];               // the fn body can operate on an abstract list instead of specific values, generics allow code to operate on abstract types. 
+// 2) Finding the largest number in a list abstractly       
+
+    fn largest (list: &[i32]) -> i32 {           //[i32] represents any concrete slice of i32 values -
+        let mut largest= list [0];               //the fn body can operate on an abstract list instead of specific values, generics allow code to operate on abstract types -
 
         for &item in list.iter() {
             if item > largest {
@@ -34,21 +35,22 @@ To eliminate this duplication, create an abstraction by defining a function that
         
         largest
     }
-   
-    let number_list= vec! [30, 400, 500, 200, 800];     // override previous number_list 
+    
+    let number_list= vec! [30, 400, 500, 200, 800];             //override previous number_list - 
     let result= largest (&number_list); 
     println! ("The largest number is {}", result); 
 
-    let number_list_2= vec! [200, 300, 400, 100, 33];   // second list number_list_2
-    let result2= largest (&number_list); 
+    let number_list_2= vec! [200, 300, 400, 100, 33];           //second list number_list_2 - 
+    let result2= largest (&number_list_2); 
     println! ("The largest number in the second list is {}", result2);
 
-// 3) Generic function for lists of different types     --------------------------------------------------------------------------------  
-    fn generic_largest <T: PartialOrd + Copy> (list: &[T]) -> T {       // the fn generic_largest is generic over some type T of the list parameter that is a slice of values of type T 
-        let mut largest= list [0];      // to move the value out of list[0], the type needs to implement the Copy trait (to be stored on the stack) 
+// 3) Generic function for lists of different types:
+
+    fn generic_largest <T: PartialOrd + Copy> (list: &[T]) -> T {           //the fn generic_largest is generic over some type T of the list parameter that is a slice of values of type T - 
+        let mut largest= list [0];              //to move the value out of list[0], the type needs to implement the Copy trait (to be stored on the stack) -  
 
         for &item in list.iter() {
-            if item > largest {         // T implements the PartialOrd trait to be able to compare like this (std::cmp::PartialOrd)
+            if item > largest {             //T implements the PartialOrd trait to be able to compare like this (std::cmp::PartialOrd) - 
                 largest= item; 
             }
         }
@@ -63,6 +65,6 @@ To eliminate this duplication, create an abstraction by defining a function that
     println! ("The largest char is {}", result_char); 
 
 /* 
-- Using the Clone trait instead of Copy - potentially making more heap allocations which can be slow.
-- Another solution is changing the return value to &T instead of T - so we dont need any other traits nor heap allocations.  */ 
+- Using the Clone trait instead of Copy: potentially making more heap allocations which can be slow.
+- Another solution is changing the return value to &T instead of T, so we dont need any other traits nor heap allocations.  */ 
 }
