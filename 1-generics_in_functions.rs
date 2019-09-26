a4568f889ae07fe93db2a18761dd53a398c62afa
Julio <jcabdu@outlook.com>
2019-09-26 17:14:30 +0200

    Update 1-generics_in_functions.rs

--- 1-generics_in_functions.rs
+++ 1-generics_in_functions.rs
@@ -1,7 +1,5 @@
-/* GENERIC TYPES: 
-- to remove duplication of code by extracting a function.   */
-
-
+// GENERIC TYPES
+// Remove duplication of code by extracting a function
 
 // 1) Short program that finds the largest number in a list:
 
@@ -18,18 +16,16 @@ fn main() {
     
     println! ("The largest number is {}", largest_number);
     
-/* To consider a second list, it is possible to duplicate the previous code but that's tedious and error prone. 
-To eliminate this duplication, create an abstraction by defining a function that operates on any list of integers: 
-1. Identify duplicate code.
-2. Extract the duplicate code into the body of the function and specify the inputs and return values of that code in the function signature.
-3. Update the two instances of duplicated code to call the function instead.    
-*/ 
-    
-    
+// To consider a second list, it is possible to duplicate the previous code but that's tedious and error prone. 
+// To eliminate this duplication, create an abstraction by defining a function that operates on any list of integers: 
+//  1. Identify duplicate code.
+//  2. Extract the duplicate code into the body of the function and specify the inputs and return values of that code in the function signature.
+//  3. Update the two instances of duplicated code to call the function instead.    
+
 // 2) Finding the largest number in a list abstractly       
 
-    fn largest (list: &[i32]) -> i32 {      //[i32] represents any concrete slice of i32 values -
-        let mut largest= list [0];      //the fn body can operate on an abstract list instead of specific values, generics allow code to operate on abstract types -
+    fn largest (list: &[i32]) -> i32 {           //[i32] represents any concrete slice of i32 values -
+        let mut largest= list [0];               //the fn body can operate on an abstract list instead of specific values, generics allow code to operate on abstract types -
 
         for &item in list.iter() {
             if item > largest {
@@ -48,13 +44,11 @@ To eliminate this duplication, create an abstraction by defining a function that
     let result2= largest (&number_list_2); 
     println! ("The largest number in the second list is {}", result2);
 
-    
-    
-/* 3) Generic function for lists of different types:
-- fn name <T: trait bounds> (arguments) -> return-value
-*/ 
-    fn generic_largest <T: PartialOrd + Copy> (list: &[T]) -> T {       //the fn generic_largest is generic over some type T of the list parameter that is a slice of values of type T - 
-        let mut largest= list [0];      //to move the value out of list[0], the type needs to implement the Copy trait (to be stored on the stack) -  
+// 3) Generic function for lists of different types:
+//  fn name <T: trait bounds> (arguments) -> return-value
+
+    fn generic_largest <T: PartialOrd + Copy> (list: &[T]) -> T {           //the fn generic_largest is generic over some type T of the list parameter that is a slice of values of type T - 
+        let mut largest= list [0];              //to move the value out of list[0], the type needs to implement the Copy trait (to be stored on the stack) -  
 
         for &item in list.iter() {
             if item > largest {             //T implements the PartialOrd trait to be able to compare like this (std::cmp::PartialOrd) - 
@@ -71,7 +65,6 @@ To eliminate this duplication, create an abstraction by defining a function that
     let result_char= generic_largest (&char_list);
     println! ("The largest char is {}", result_char); 
 
-/* 
-- Using the Clone trait instead of Copy: potentially making more heap allocations which can be slow.
-- Another solution is changing the return value to &T instead of T, so we dont need any other traits nor heap allocations.  */ 
+// Using the Clone trait instead of Copy: potentially making more heap allocations which can be slow.
+// Another solution is changing the return value to &T instead of T, so we dont need any other traits nor heap allocations.
 }
